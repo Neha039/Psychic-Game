59b2271474ae0107e669fdc6c77fa42d8e8d4ae0
Neha Nehru <neha_nehru@hotmail.com>
2020-01-28 21:29:00 -0800

    finished game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,4 @@
-// var numWin = 0;
-// var numLoss = 0;
-// var numGuess = 9;
-
-// function numberWin() {
-//     numWin += numWin;
-// }
-
-// function numberLoss() {
-//     numLoss += numLoss;
-// }
-
-// function guessGame(){
-//     //computers random guesses
-//     var compGuess;
-//     // get user to enter 9 guesses
-//     var userGuess;
-
-//     for(var i = 0; i < 10; i++) {
-//         if(userGuess == compGuess) {
-//             numWin++;
-//         }
-//         else {
-//             numLoss++; 
-//         }   
-        
-//     }
-// }
-
-// console.log("End of Game");
-
-//Setting variables and zeroing counters (`var attempts` set to 9 yeilds 10 total attempts as numbering begins with zero)
+//Creating variables and setting them to default 
 var won = 0;
 var lost = 0;
 var attempts = 10;
@@ -37,7 +6,6 @@ var usedArray = [];
 var ranLetter = ranLetter;
 var letters = "qwertyuiopasdfghjklzxcvbnm"
 
-//GENERATING THE COMPUTERS SECRET LETTER
 //Generates random number (decimal from 0 to 1) which is multiplied by 26 and the rounded down to the nearest whole number by the Math.floor method. 
 ranLetter = letters[Math.floor(Math.random() * letters.length)];
 console.log(ranLetter);
@@ -49,27 +17,22 @@ function jsGuess() {
 
 }
 
-//CAPTURING THE PLAYERS INPUT
 //.onkeyup captures the players input as playerGuess
 document.onkeyup = function (event) {
       var playerGuess = event.key;
 
       // HANDELING CORRECT GUESSES
-      //test if players guess equals ranLetter, if true it increments wins by 1, and clears used letters array. Supposed to reset guess # to 10 but starts at 9 instead, 
-      if (playerGuess === ranLetter) {
+       if (playerGuess === ranLetter) {
             won++;
             attempts = 10;
             usedArray = [];
-
-      }
+        }
 
       //HANDELING INCORRECT GUESSES
-      //tests if players guess Does Not Equal ranLetter and decriments attempts by 1
       jsGuess();
       if (playerGuess !== ranLetter) {
             attempts--;
-
-      }
+        }
 
       //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
       if (attempts == 0) {
@@ -79,7 +42,6 @@ document.onkeyup = function (event) {
       }
 
       //HANDELING INCORRECT GUESSES - OUTPUT
-      //this 'if' prevents a letter selected a 2nd time from being written to the usedArray again, although it still counts as a guess
       if (usedArray.indexOf(playerGuess) >= 0) {
 
       } else {
@@ -90,7 +52,6 @@ document.onkeyup = function (event) {
 
       }
       //OUTPUT TO HTML
-      //these statements write the values/scores generated to the HTML
       document.getElementById('won').innerHTML = won;
       document.getElementById('lost').innerHTML = lost;
       document.getElementById('attempts').innerHTML = attempts;
